7e531102a123e5d42aea481a4f382548576fcacf
Afsana-Tasnim-Juha <111423065+Afsana-Tasnim-Juha@users.noreply.github.com>
2023-08-13 12:13:40 +0200

    first TASK

---
+++ task1.js
@@ -0,0 +1,8 @@
+function cubeNumber(number) {
+
+
+    return number * number * number;
+
+}
+const result = cubeNumber(4);
+console.log(result);
---
+++ task2.js
@@ -0,0 +1,13 @@
+function matchFinder(string1, string2) {
+
+    if (string1.indexOf(string2) !== -1) {
+        return true;
+    } else {
+        return false;
+    }
+}
+const string1 = 'Peter Parker';
+const string2 = 'Code';
+
+const findString = matchFinder(string1, string2);
+console.log(findString);
